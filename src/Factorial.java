661921cdd444caf127d20bcf7cba0a87c9c18d71
Moahmed Elgebaly <elgebalym2082000@gmail.com>
2023-10-21 17:12:38 +0300

    facorial problem

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Factorial {
-    public static void main(String[] args) {
+     static int factorial(int x){
+        if(x==1)
+         return 1;
+        else
+         return x*factorial(x-1);
 
     }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+       System.out.println(factorial(x));
+
+       /*   Another Solution */
+       System.out.println("/**********************    Another Solution  **********************/  ");
+       int number  = scanner.nextInt();
+       int result = 1;
+       for(int i = 1 ; i<=number ; i++){
+            result = result*i;
+       }
+       System.out.println("factorial of "+number+" = "+result);
+        
+       
+    }
+   
+
 }

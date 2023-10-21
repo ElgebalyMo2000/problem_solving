881e989ef5e862977698914334f2c0bf2a8df7e3
Moahmed Elgebaly <elgebalym2082000@gmail.com>
2023-10-21 16:34:53 +0300

    last update

--- src/Watermelon.java
+++ src/Watermelon.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Watermelon {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
+            double weight = scanner.nextDouble();
 
-        double weight = scanner.nextDouble();
-
-        if(((weight % 2) ==0) && (weight >2))
-            System.out.println("Yes");
-        else
-            System.out.println("No");
+            if(((weight % 2) ==0) && (weight >2))
+                System.out.println("Yes");
+            else
+                System.out.println("No");
+        }
 
     }
     }
